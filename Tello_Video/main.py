bd3f193f114bb2ee6d3a448898b9dbe3bd637b16
Josh-Alsybury <Joshbarcoealsybury@outlook.ie>
2026-03-19 12:20:12 +0000

    preped code for drone test

--- Tello_Video/main.py
+++ Tello_Video/main.py
@@ -1,14 +1,18 @@
 import tello
 from tello_control_ui import TelloUI
+import os
 
 
 def main():
 
-    drone = tello.Tello('', 8889)  
-    vplayer = TelloUI(drone,"./img/")
-    
-	# start the Tkinter mainloop
-    vplayer.root.mainloop() 
+    # make sure img folder exists so snapshots don't crash
+    os.makedirs('./img', exist_ok=True)
+
+    drone = tello.Tello('0.0.0.0', 8889)  # fixed empty string
+    vplayer = TelloUI(drone, "./img/")
+
+    # start the Tkinter mainloop
+    vplayer.root.mainloop()
 
 if __name__ == "__main__":
     main()
